e3819afec244e2409c6bae7c00641a1b6fca0c91
huysg136 <thaigiahuy6912@gmail.com>
2025-11-18 15:16:36 +0700

    ex03_01

--- lab_01/ex03/ex03_01.py
+++ lab_01/ex03/ex03_01.py
@@ -1,1 +1,12 @@
-#ccc
+def tinh_tong_so_chan(lst):
+    tong = 0
+    for so in lst:
+        if so % 2 == 0:
+            tong += so
+    return tong
+
+input_list = input("Nhập danh sách số nguyên, cách nhau bằng dấu phẩy: ")
+numbers = list(map(int, input_list.split(',')))
+
+tong_so_chan = tinh_tong_so_chan(numbers)
+print("Tổng các số chẵn trong danh sách là:", tong_so_chan)
